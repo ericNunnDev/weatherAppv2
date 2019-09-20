573c58a575b3f874b0dbfae04f25c2cb8f147211
lowlyHero <e.nunn1009@gmail.com>
2019-09-20 11:39:49 -0400

    Create-react-app installed and files cleaned

---
+++ view/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+    </div>
+  );
+}
+
+export default App;
