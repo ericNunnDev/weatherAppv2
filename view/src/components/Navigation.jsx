831e95427374f5de5c73c1f96852e946a7611b8d
lowlyHero <e.nunn1009@gmail.com>
2019-09-20 19:08:55 -0400

    Centered Nav content on mobile view. Added other features to navbar

--- view/src/components/Navigation.jsx
+++ view/src/components/Navigation.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 const Navigation = () => {
     return (
         <div className='nav-wrapper'>
-            <a href='#' className='brand-logo'>What the Weather?</a>
-            <ul className='right'>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>About</a></li>
-                <li><a href='#'>Contact</a></li>
-            </ul>
+            <div className='container'>
+                <div className='row'>
+                  <div className='col s12'>
+                      <div>
+                        <a href='#' className='brand-logo left'><i className='fas fa-cloud-meatball fa-lg' /> What the Weather?</a>
+                      </div>
+                    <ul className='right'>
+                        <li><a className='nav-link' href='#'>Home</a></li>
+                        <li><a className='nav-link' href='#'>About</a></li>
+                        <li><a className='nav-link' href='#'>Contact</a></li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
         </div>
     )
 }
