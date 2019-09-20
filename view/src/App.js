f66ff0ff6169f6fb99f5d638436de7d4fc6ca8bb
lowlyHero <e.nunn1009@gmail.com>
2019-09-20 12:30:01 -0400

    Cleaning files. Creating file structure for components

--- view/src/App.js
+++ view/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../src/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-    </div>
+  return ( 
+  <div className = "App" >
+    <header>
+      <h1> What's the Weather?</h1> 
+    </header>
+    <main>
+      <div id='search'>
+        <label htmlFor='city'>City Name: </label>
+        <input type='text' id='city' />
+        <button type='submit'>Show me the Weather!</button>
+      </div>
+      <div>
+        <h1 id='weatherCity'>City Name</h1>
+        <div id='weatherDesc'>Weather Description</div>
+        <div id='weatherTemp'>Temperature</div>
+      </div>
+    </main>
+    <footer>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </footer>
+  </div>
   );
 }
 
---
+++ view/src/components/init.js
@@ -0,0 +1,7 @@
+const searchButton = document.querySelector('button'),
+      searchCity = document.getElementById('city'),
+      loadingText = document.getElementById('load'),
+      weatherBox = document.getElementById('weather'),
+      weatherCity = weatherBox.firstElementChild,
+      weatherDescription = document.getElementById('weatherDescription'),
+      weatherTemp = weatherBox.lastElementChild;
